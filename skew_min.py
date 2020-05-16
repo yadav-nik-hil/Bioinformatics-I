59676256d484a1e5e0f00085d831e1753ba1385e
NIKHIL YADAV <53780673+yadav-nik-hil@users.noreply.github.com>
2020-05-16 14:39:26 +0530

    Add files via upload

---
+++ skew_min.py
@@ -0,0 +1,23 @@
+sequence = 'GATACACTTCCCGAGTAGGTACTG'
+
+def skew(sequence):
+	c = 0
+	g = 0
+	min_skew = 0
+	skew_list = []
+	index = 0
+	for i in sequence:
+		index += 1
+		if i == 'C':
+			c += 1
+		if i == 'G':
+			g += 1
+		skew = g-c
+		if skew < min_skew:
+			skew_list = [index]
+			min_skew = skew
+		if skew == min_skew and index not in skew_list:
+			skew_list.append(index)	
+	print(skew_list)
+
+skew(sequence)
